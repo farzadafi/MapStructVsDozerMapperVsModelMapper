f2def4fba2309a2b3867080b11ab0d4f86193bfc
farzadafi <farzadafi40@gmail.com>
2022-08-24 10:09:17 +0430

    add AllArgsConstructor

--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -4,4 +4,10 @@ public class Address {
     private String county;
     private String province;
     private String city;
+
+    public Address(String county, String province, String city) {
+        this.county = county;
+        this.province = province;
+        this.city = city;
+    }
 }
