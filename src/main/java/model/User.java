2f708d3b338d613382005181332aceed8a535401
farzadafi <farzadafi40@gmail.com>
2022-08-24 10:06:21 +0430

    add Address field and one list

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.util.List;
+
 public class User {
     private String firstName;
     private String lastName;
     private String userName;
     private String password;
     private String email;
+    private Address address;
+    private List<String> favoriteSubject;
 }
