0fc1396fbac031f91dcd553321a67d894d78cd18
farzadafi <farzadafi40@gmail.com>
2022-08-24 10:04:18 +0430

    add some need field

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,9 @@
 package model;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
 }
