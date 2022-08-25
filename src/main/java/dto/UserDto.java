097676ae27b49227dc9cdd2df916f64dc21651c2
farzadafi <farzadafi40@gmail.com>
2022-08-25 20:19:17 +0430

    add some need field to class

--- src/main/java/dto/UserDto.java
+++ src/main/java/dto/UserDto.java
@@ -1,4 +1,15 @@
 package dto;
 
+import model.Address;
+
+import java.util.List;
+
 public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String email;
+    private Address address;
+    private List<String> favoriteSubject;
 }
