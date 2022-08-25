640f5cb3435883f9e77261aab04d7cccb66e4700
farzadafi <farzadafi40@gmail.com>
2022-08-25 19:12:50 +0430

    rename to DozerMapperImpel and implement from DozerMapper

---
+++ src/main/java/mapper/dozerMapper/DozerMapperImpel.java
@@ -0,0 +1,10 @@
+package mapper.dozerMapper;
+
+public class DozerMapperImpel implements DozerMapper {
+
+    private final DozerMapperImpel dozerMapper;
+
+    public DozerMapperImpel() {
+        this.dozerMapper = new DozerMapperImpel();
+    }
+}
