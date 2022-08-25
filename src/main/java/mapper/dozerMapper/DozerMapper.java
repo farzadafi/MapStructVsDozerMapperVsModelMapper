b5288efc8fbb69c23e65f01d0cab46840ef5ac6e
farzadafi <farzadafi40@gmail.com>
2022-08-25 20:24:03 +0430

    add dtoToModel method

--- src/main/java/mapper/dozerMapper/DozerMapper.java
+++ src/main/java/mapper/dozerMapper/DozerMapper.java
@@ -1,4 +1,8 @@
 package mapper.dozerMapper;
 
+import dto.UserDto;
+import model.User;
+
 public interface DozerMapper {
+    User dtoToModel(UserDto user);
 }
--- src/main/java/mapper/dozerMapper/DozerMapperImpel.java
+++ src/main/java/mapper/dozerMapper/DozerMapperImpel.java
@@ -1,10 +1,19 @@
 package mapper.dozerMapper;
 
+import dto.UserDto;
+import model.User;
+import org.dozer.DozerBeanMapper;
+
 public class DozerMapperImpel implements DozerMapper {
 
-    private final DozerMapperImpel dozerMapper;
+    private final DozerBeanMapper mapper;
 
     public DozerMapperImpel() {
-        this.dozerMapper = new DozerMapperImpel();
+        mapper = new DozerBeanMapper();
+    }
+
+    @Override
+    public User dtoToModel(UserDto userDto) {
+        return mapper.map(userDto,User.class);
     }
 }
