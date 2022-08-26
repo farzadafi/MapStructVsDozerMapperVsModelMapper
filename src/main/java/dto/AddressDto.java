7767744acc2331ce13ef94994694badc84c48d44
farzadafi <farzadafi40@gmail.com>
2022-08-26 07:06:33 +0430

    add some field and add lombok annotation

--- src/main/java/dto/AddressDto.java
+++ src/main/java/dto/AddressDto.java
@@ -1,4 +1,14 @@
 package dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class AddressDto {
+    private String county;
+    private String province;
+    private String city;
 }
