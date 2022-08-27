053acc007b62f158ed373569251b041700c84a98
farzadafi <farzadafi40@gmail.com>
2022-08-27 13:35:13 +0430

    add dtoToModel method

--- src/main/java/mapper/modelMapper/ModelMapper.java
+++ src/main/java/mapper/modelMapper/ModelMapper.java
@@ -1,4 +1,8 @@
 package mapper.modelMapper;
 
+import dto.UserDto;
+import model.User;
+
 public interface ModelMapper {
+    User dtoToModel(UserDto user);
 }
--- src/main/java/mapper/modelMapper/ModelMapperImpel.java
+++ src/main/java/mapper/modelMapper/ModelMapperImpel.java
@@ -1,4 +1,18 @@
 package mapper.modelMapper;
 
-public class ModelMapperImpel {
+import dto.UserDto;
+import model.User;
+
+public class ModelMapperImpel implements ModelMapper{
+
+    private final org.modelmapper.ModelMapper mapper;
+
+    public ModelMapperImpel(){
+        mapper = new org.modelmapper.ModelMapper();
+    }
+
+    @Override
+    public User dtoToModel(UserDto userDto) {
+        return mapper.map(userDto,User.class);
+    }
 }
