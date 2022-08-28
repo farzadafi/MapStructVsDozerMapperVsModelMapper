f0e1c463c5158ccd0c278106686aab859934e49b
farzadafi <farzadafi40@gmail.com>
2022-08-28 15:30:53 +0430

    add dtoListToModelList method

--- src/main/java/mapper/mapStruct/MapStruct.java
+++ src/main/java/mapper/mapStruct/MapStruct.java
@@ -5,10 +5,14 @@ import model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface MapStruct {
 
     MapStruct INSTANCE = Mappers.getMapper(MapStruct.class);
 
     User dtoToModel(UserDto userDto);
+    UserDto modelToDto(User user);
+    List<User> dtoListToModelList(List<UserDto> userDtoList);
 }
