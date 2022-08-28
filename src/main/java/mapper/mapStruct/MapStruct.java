39af7dac2dbd352b694b914e991851c179a3b223
farzadafi <farzadafi40@gmail.com>
2022-08-28 15:09:32 +0430

    add dtoToModel method

--- src/main/java/mapper/mapStruct/MapStruct.java
+++ src/main/java/mapper/mapStruct/MapStruct.java
@@ -1,4 +1,14 @@
 package mapper.mapStruct;
 
+import dto.UserDto;
+import model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface MapStruct {
+
+    MapStruct INSTANCE = Mappers.getMapper(MapStruct.class);
+
+    User dtoToModel(UserDto userDto);
 }
